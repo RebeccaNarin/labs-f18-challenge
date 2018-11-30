802b70dc2f27b2558f597ce5c561807544043978
RebeccaNarin <45371424+RebeccaNarin@users.noreply.github.com>
2018-11-30 14:42:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+import requests
+>>> import pykemon
+>>> client = pykemon.V1Client()
+>>> p = client.get_pokemon(uid=1)
+[<Pokemon - Bulbasaur>]
 
-app = Flask(__name__)
+#using json
+#Get parameters for the //
+response = 
+request.get("http://pokeapi.co/api/v2/pokemon/", response)
+
+response.data
 
 
 @app.route('/', methods=['GET'])
+
+@app.route('/pokemon'
 def main():
     return render_template('index.html')
 
